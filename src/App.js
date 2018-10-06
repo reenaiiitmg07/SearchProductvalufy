1491b52bb3808d6648197f845dfd5a7ba7072658
reenavermaiiitm <reenavermaiiitm@gmail.com>
2018-10-06 16:00:21 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
       {stocks.map((item)=>{
         return(
           <li>
-          <div className="card" style={{border: "1px solid gray"}}>
+          <div className="card" style={{border: "1px solid gray",width: "200px",height: "220px",padding:"10px 5px 5px 10px"}}>
              Symbol:{item.SYMBOL}
                <h6>Date:{item.DATE1}</h6>
                <p>Series:{item.SERIES}</p>
--- src/component/search.js
+++ src/component/search.js
@@ -4,17 +4,27 @@ import {connect} from 'react-redux';
 class Search extends Component {
   constructor(props){
     super();
+    this.state={
+      term:" "
+    }
+    this.onChangeHandle=this.onChangeHandle.bind(this);
     this.onSubmitHandle=this.onSubmitHandle.bind(this);
   }
   onSubmitHandle(e){
     e.preventDefault();
-    console.log(e.target.value);
+    console.log(this.state.term);
+    console.log(e.target.name);
+  }
+  onChangeHandle(e){
+   this.setState({
+     term:e.target.value
+   });
   }
   render(){
     return(
       <div className="row" style={{ textAlign: 'center', padding: '10px' }}>
       <form  onsubmit={this.onSubmitHandle}>
-        Search: <input type="text" name="search"/>
+        Search: <input type="text" onChange={this.onChangeHandle} name="search"/>
        <input type="submit" value="Submit"/>
      </form>
 </div>
